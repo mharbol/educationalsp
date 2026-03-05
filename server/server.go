b34d5de0926c80fc6841ee6014d062793108a80b
Marcus Harbol <mharbol@users.noreply.github.com>
2026-03-04 23:08:15 -0700

    break up server Serve()

--- server/server.go
+++ server/server.go
@@ -36,7 +36,7 @@ func (s *Server) Serve() {
 			s.Logger.Printf("Error decoding message: %s", err)
 			continue
 		}
-		s.HandleMessage(method, contents)
+		s.handleMessage(method, contents)
 	}
 }
 
@@ -52,48 +52,48 @@ func getLogger(filename string) *log.Logger {
 	return log.New(logfile, "[educationalsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
-func (s *Server) HandleMessage(method string, contents []byte) {
+func (s *Server) handleMessage(method string, contents []byte) {
 	s.Logger.Printf("RECV msg with method: %s", method)
 	switch method {
 	case "initialize":
-		var request lsp.InitializeRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			s.Logger.Printf("initialize error: %s", err)
-		}
-		s.Logger.Printf("Connected to: %s %s",
-			request.Params.ClientInfo.Name,
-			request.Params.ClientInfo.Version)
-		msg := lsp.NewInitializeResponse(request.ID)
-		s.WriteMessage(&msg)
-		s.Logger.Printf("Sent reply")
+		s.handleInitialize(contents)
 	case "textDocument/didChange":
-		var notification lsp.TextDocumentDidChangeNotification
-		if err := json.Unmarshal(contents, &notification); err != nil {
-			s.Logger.Printf("textDocument/didChange error: %s", err)
-		}
-		s.Logger.Printf("Changed: %s", notification.Params.TextDocument.URI)
-		for _, change := range notification.Params.ContentChanges {
-			// hook in diagnostics here...
-			diagnostics := s.State.UpdateDocument(notification.Params.TextDocument.URI, change.Text)
-			s.WriteMessage(lsp.PublishDiagnosticsNotification{
-				Notification: lsp.Notification{
-					RPC:    lsp.JSONRPC,
-					Method: "textDocument/publishDiagnostics",
-				},
-				Params: lsp.PublishDiagnosticsParams{
-					URI:         notification.Params.TextDocument.URI,
-					Diagnostics: diagnostics,
-				},
-			})
-		}
+		s.handleDidChange(contents)
 	case "textDocument/didOpen":
-		var notification lsp.DidOpenTextDocumentNotification
-		if err := json.Unmarshal(contents, &notification); err != nil {
-			s.Logger.Printf("textDocument/didOpen error: %s", err)
-		}
-		s.Logger.Printf("Opened: %s", notification.Params.TextDocument.URI)
-		// hook in diagnostics here too...
-		diagnostics := s.State.OpenDocument(notification.Params.TextDocument.URI, notification.Params.TextDocument.Text)
+		s.handleDidOpen(contents)
+	case "textDocument/hover":
+		s.handleHover(contents)
+	case "textDocument/definition":
+		s.handleDefinition(contents)
+	case "textDocument/codeAction":
+		s.handleCodeAction(contents)
+	case "textDocument/completion":
+		s.handleCompletion(contents)
+	}
+}
+
+func (s *Server) handleInitialize(contents []byte) {
+	var request lsp.InitializeRequest
+	if err := json.Unmarshal(contents, &request); err != nil {
+		s.Logger.Printf("initialize error: %s", err)
+	}
+	s.Logger.Printf("Connected to: %s %s",
+		request.Params.ClientInfo.Name,
+		request.Params.ClientInfo.Version)
+	msg := lsp.NewInitializeResponse(request.ID)
+	s.WriteMessage(&msg)
+	s.Logger.Printf("Sent reply")
+}
+
+func (s *Server) handleDidChange(contents []byte) {
+	var notification lsp.TextDocumentDidChangeNotification
+	if err := json.Unmarshal(contents, &notification); err != nil {
+		s.Logger.Printf("textDocument/didChange error: %s", err)
+	}
+	s.Logger.Printf("Changed: %s", notification.Params.TextDocument.URI)
+	for _, change := range notification.Params.ContentChanges {
+		// hook in diagnostics here...
+		diagnostics := s.State.UpdateDocument(notification.Params.TextDocument.URI, change.Text)
 		s.WriteMessage(lsp.PublishDiagnosticsNotification{
 			Notification: lsp.Notification{
 				RPC:    lsp.JSONRPC,
@@ -104,33 +104,61 @@ func (s *Server) HandleMessage(method string, contents []byte) {
 				Diagnostics: diagnostics,
 			},
 		})
-	case "textDocument/hover":
-		var request lsp.HoverRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			s.Logger.Printf("textDocument/hover error: %s", err)
-		}
-		response := s.State.Hover(request.ID, request.Params.TextDocument.URI, request.Params.Position)
-		s.WriteMessage(&response)
-	case "textDocument/definition":
-		var request lsp.DefinitionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			s.Logger.Printf("textDocument/definition error: %s", err)
-		}
-		response := s.State.Definition(request.ID, request.Params.TextDocument.URI, request.Params.Position)
-		s.WriteMessage(&response)
-	case "textDocument/codeAction":
-		var request lsp.CodeActionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			s.Logger.Printf("textDocument/codeAction error: %s", err)
-		}
-		response := s.State.TextDocumentCodeAction(request.ID, request.Params.TextDocument.URI)
-		s.WriteMessage(&response)
-	case "textDocument/completion":
-		var request lsp.CompletionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			s.Logger.Printf("textDocument/completion error: %s", err)
-		}
-		response := s.State.TextDocumentCompletion(request.ID, request.Params.TextDocument.URI)
-		s.WriteMessage(&response)
 	}
+}
+
+func (s *Server) handleDidOpen(contents []byte) {
+	var notification lsp.DidOpenTextDocumentNotification
+	if err := json.Unmarshal(contents, &notification); err != nil {
+		s.Logger.Printf("textDocument/didOpen error: %s", err)
+	}
+	s.Logger.Printf("Opened: %s", notification.Params.TextDocument.URI)
+	// hook in diagnostics here too...
+	diagnostics := s.State.OpenDocument(notification.Params.TextDocument.URI, notification.Params.TextDocument.Text)
+	s.WriteMessage(lsp.PublishDiagnosticsNotification{
+		Notification: lsp.Notification{
+			RPC:    lsp.JSONRPC,
+			Method: "textDocument/publishDiagnostics",
+		},
+		Params: lsp.PublishDiagnosticsParams{
+			URI:         notification.Params.TextDocument.URI,
+			Diagnostics: diagnostics,
+		},
+	})
+}
+
+func (s *Server) handleHover(contents []byte) {
+	var request lsp.HoverRequest
+	if err := json.Unmarshal(contents, &request); err != nil {
+		s.Logger.Printf("textDocument/hover error: %s", err)
+	}
+	response := s.State.Hover(request.ID, request.Params.TextDocument.URI, request.Params.Position)
+	s.WriteMessage(&response)
+}
+
+func (s *Server) handleDefinition(contents []byte) {
+	var request lsp.DefinitionRequest
+	if err := json.Unmarshal(contents, &request); err != nil {
+		s.Logger.Printf("textDocument/definition error: %s", err)
+	}
+	response := s.State.Definition(request.ID, request.Params.TextDocument.URI, request.Params.Position)
+	s.WriteMessage(&response)
+}
+
+func (s *Server) handleCodeAction(contents []byte) {
+	var request lsp.CodeActionRequest
+	if err := json.Unmarshal(contents, &request); err != nil {
+		s.Logger.Printf("textDocument/codeAction error: %s", err)
+	}
+	response := s.State.TextDocumentCodeAction(request.ID, request.Params.TextDocument.URI)
+	s.WriteMessage(&response)
+}
+
+func (s *Server) handleCompletion(contents []byte) {
+	var request lsp.CompletionRequest
+	if err := json.Unmarshal(contents, &request); err != nil {
+		s.Logger.Printf("textDocument/completion error: %s", err)
+	}
+	response := s.State.TextDocumentCompletion(request.ID, request.Params.TextDocument.URI)
+	s.WriteMessage(&response)
 }
