5390208518c86f956f30e79b5bc4fc92a68bfd3c
Marcus Harbol <mharbol@users.noreply.github.com>
2026-03-06 14:44:28 -0700

    comments and javadoc

--- src/main/java/org/lbpl/lsp/rpc/RpcReader.java
+++ src/main/java/org/lbpl/lsp/rpc/RpcReader.java
@@ -12,17 +12,42 @@ public class RpcReader implements AutoCloseable {
     private final InputStream inputStream;
     private final BufferedReader bufferedReader;
 
+    /**
+     * LSP header consistis of just "Content-Length: <number>"
+     */
+    private static final String CONTENT_LENGTH_STR = "Content-Length: ";
+
+    /**
+     * Length of CONTENT_LENGTH_STR for substring-ing
+     */
+    private static final int CONTENT_LENGTH_STR_LEN = CONTENT_LENGTH_STR.length();
+
+    /**
+     * Regex to verify a line reading "Content-Length: <number>"
+     */
+    private static final String CONTENT_LENGTH_REGEX = "^" + CONTENT_LENGTH_STR + "[0-9]+$";
+
+    /**
+     * Reader specifically made to read LSP messages from a client.
+     * 
+     * @param inputStream
+     */
     public RpcReader(InputStream inputStream) {
         this.inputStream = inputStream;
         this.bufferedReader = new BufferedReader(new InputStreamReader(this.inputStream, StandardCharsets.UTF_8));
     }
 
+    /**
+     * Read the next JSON {@link String} from the client
+     * 
+     * @return the next JSON {@link String} from the client
+     */
     public String readRpcJson() {
         try {
             String line;
             if ((line = bufferedReader.readLine()) != null) {
-                if (line.matches("^Content-Length: [0-9]+$")) {
-                    String sz = line.substring("Content-Length: ".length());
+                if (line.matches(CONTENT_LENGTH_REGEX)) {
+                    String sz = line.substring(CONTENT_LENGTH_STR_LEN);
                     bufferedReader.read(); // \r
                     bufferedReader.read(); // \n
                     CharBuffer buffer = CharBuffer.allocate(Integer.parseInt(sz));
